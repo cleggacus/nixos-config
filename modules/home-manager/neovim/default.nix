4216025fa32e9ea649926c841e3e9ea9edce7198
cleggacus <cleggacus@gmail.com>
2025-07-05 23:42:26 +0100

    nvim

---
+++ modules/home-manager/neovim/default.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      lazy-nvim
+      which-key-nvim
+    ];
+
+    extraLuaConfig = ''
+      vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
+      require("lazy").setup({
+        performance = {
+          reset_packpath = false,
+          rtp = {
+              reset = false,
+            }
+          },
+        dev = {
+          path = "${pkgs.vimUtils.packDir config.home-manager.users.USERNAME.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
+          patterns = {""}, -- Specify that all of our plugins will use the dev dir. Empty string is a wildcard!
+        },
+        install = {
+          -- Safeguard in case we forget to install a plugin with Nix
+          missing = false,
+        },
+        spec = {
+          -- Import plugins from lua/plugins
+          { import = "plugins" },
+        },
+      })
+    '';
+
+    xdg.configFile."nvim/lua" = {
+      recursive = true;
+      source = ./lua;
+    };
+  };
+}
