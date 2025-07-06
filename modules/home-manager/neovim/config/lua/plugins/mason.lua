9cc9aa20d6b7c0be1575f212f79509639a055494
cleggacus <cleggacus@gmail.com>
2025-07-06 02:36:35 +0100

    Add Neovim config directory

---
+++ modules/home-manager/neovim/config/lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+  "mason.nvim",
+  opts = { 
+    ensure_installed = { 
+      "cmakelang", 
+      "cmakelint", 
+      "js-debug-adapter",
+      "codelldb",
+      "bacon",
+    },
+  },
+}
