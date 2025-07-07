a5d9e1f3140b42f0f3986b34612ee06517fd35bf
cleggacus <cleggacus@gmail.com>
2025-07-07 15:05:05 +0100

    nvim update

--- modules/home-manager/neovim/config/lua/plugins/colorscheme.lua
+++ modules/home-manager/neovim/config/lua/plugins/colorscheme.lua
@@ -1,12 +1,8 @@
 return {
-  -- add dracula
-  { 
-    "Mofiqul/dracula.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme dracula]])
-    end,
-  },
+  "Mofiqul/dracula.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    vim.cmd([[colorscheme dracula]])
+  end,
 }
