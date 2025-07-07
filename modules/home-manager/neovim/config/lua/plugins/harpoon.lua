77fce58b441f81503d083d4ecf784ed20b8fa827
cleggacus <cleggacus@gmail.com>
2025-07-07 15:33:50 +0100

    nvim harpoon

--- modules/home-manager/neovim/config/lua/plugins/harpoon.lua
+++ modules/home-manager/neovim/config/lua/plugins/harpoon.lua
@@ -6,6 +6,7 @@ return {
     local harpoon = require('harpoon')
 
     harpoon:setup({})
+
     
     local conf = require("telescope.config").values
 
@@ -26,7 +27,45 @@ return {
       }):find()
     end
     
-    vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
-    { desc = "Open harpoon window" })
+    vim.keymap.set("n", "<leader>hv", function() toggle_telescope(harpoon:list()) end)
+    vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<leader>hr", function() harpoon:list():remove() end)
+    
+    vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<leader>h5", function() harpoon:list():select(5) end)
+    vim.keymap.set("n", "<leader>h6", function() harpoon:list():select(6) end)
+    vim.keymap.set("n", "<leader>h7", function() harpoon:list():select(7) end)
+    vim.keymap.set("n", "<leader>h8", function() harpoon:list():select(8) end)
+    vim.keymap.set("n", "<leader>h9", function() harpoon:list():select(9) end)
+    vim.keymap.set("n", "<leader>h0", function() harpoon:list():select(10) end)
+    
+    vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+
+    local wk = require('which-key')
+
+    wk.add({
+      { "<leader>h", group = "Harpoon" },
+      { "<leader>hv", desc = "Harpoon View" },
+      { "<leader>ha", desc = "Harpoon Add" },
+      { "<leader>hr", desc = "Harpoon Remove" },
+                   
+      { "<leader>h1", desc = "Harpoon 1" },
+      { "<leader>h2", desc = "Harpoon 2" },
+      { "<leader>h3", desc = "Harpoon 3" },
+      { "<leader>h4", desc = "Harpoon 4" },
+      { "<leader>h5", desc = "Harpoon 5" },
+      { "<leader>h6", desc = "Harpoon 6" },
+      { "<leader>h7", desc = "Harpoon 7" },
+      { "<leader>h8", desc = "Harpoon 8" },
+      { "<leader>h9", desc = "Harpoon 9" },
+      { "<leader>h0", desc = "Harpoon 10" },
+
+      { "<leader>hp", desc = "Previous" },
+      { "<leader>hn", desc = "Next" },
+    })   
   end,
 }
--- modules/home-manager/neovim/config/lua/plugins/telescope.lua
+++ modules/home-manager/neovim/config/lua/plugins/telescope.lua
@@ -4,12 +4,13 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
-    local wk = require('which-key')
 
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+    local wk = require('which-key')
 
     wk.add({
       { "<leader>f", group = "Telescope" },
