c55227d5c94c9abf2c2b723e1cd7922c28472984
cleggacus <cleggacus@gmail.com>
2025-07-07 17:02:08 +0100

    nvim update

--- modules/home-manager/neovim/config/lua/plugins/mason-lspconfig.lua
+++ modules/home-manager/neovim/config/lua/plugins/mason-lspconfig.lua
@@ -1,4 +1,15 @@
-return { 
-  "williamboman/mason-lspconfig.nvim", 
-  config = true,
+return {
+  "williamboman/mason-lspconfig.nvim",
+  config = function()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+        "clangd",
+        "eslint",
+        "rome",
+        "pyre"
+      }
+    })
+  end
 }
--- modules/home-manager/neovim/config/lua/plugins/mason.lua
+++ modules/home-manager/neovim/config/lua/plugins/mason.lua
@@ -1,4 +1,8 @@
 return { 
-  "williamboman/mason.nvim",
-  config = true 
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
 }
