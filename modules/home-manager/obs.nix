4440abca4e324afd3753075974cf4d27a3ac8535
cleggacus <cleggacus@gmail.com>
2025-07-18 21:56:32 +0100

    wrong package

--- modules/home-manager/obs.nix
+++ modules/home-manager/obs.nix
@@ -5,13 +5,6 @@
   programs.obs-studio = {
     enable = true;
 
-    # optional Nvidia hardware acceleration
-    package = (
-      pkgs.obs-studio.override {
-        cudaSupport = true;
-      }
-    );
-
     plugins = with pkgs.obs-studio-plugins; [
       wlrobs
       obs-backgroundremoval
